aafaa2aefc9dd70188479ed6a74780553347e967
Isailton89 <isailton2008@gmail.com>
2023-11-28 23:55:07 -0300

    criando lógica no metodo contar()

--- src/Contador.java
+++ src/Contador.java
@@ -4,9 +4,9 @@ public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
         System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.??;
+        int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.??;
+        int parametroDois = terminal.nextInt();
 
         try {
             //chamando o método contendo a lógica de contagem
@@ -19,8 +19,16 @@ public class Contador {
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm > parametroDois) {
+            int contagem = parametroDois - parametroUm;
+            for (int numero = 1; numero <= contagem; numero++) {
+                System.out.println("Imprimindo o número " + numero);
+                numero++;
+            }
+        }else {
+            throw ParametrosInvalidosException;
+        }
 
-        int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
     }
 }
